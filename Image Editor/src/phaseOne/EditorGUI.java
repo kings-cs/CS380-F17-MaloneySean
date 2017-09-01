5cab30761eddada2ef216c16371044f0fac341f5
Sean Maloney <seanmaloney@kings.edu>
2017-09-01 16:27:29 -0400

    Initial Commit.

---
+++ Image Editor/src/phaseOne/EditorGUI.java
@@ -0,0 +1,16 @@
+package phaseOne;
+
+import javax.swing.JFrame;
+
+/**
+ * Main GUI window used in an Image Editor application that will allow the user to perform various modifications on a displayed image.
+ * @author Sean Maloney
+ */
+public class EditorGUI extends JFrame{
+
+	/**
+	 * Generated Serial ID.
+	 */
+	private static final long serialVersionUID = 7320549510928224667L;
+
+}
