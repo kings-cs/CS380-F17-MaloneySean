5f88c0e78113914a320050699aeb161df5eb4add
seanmaloney <seanmaloney@ad42507>
2017-09-25 00:53:00 -0400

    Made a blur class...productive day.

---
+++ Image Editor/src/ohCrop/algorithms/Blur.java
@@ -0,0 +1,47 @@
+package ohCrop.algorithms;
+
+import java.awt.image.BufferedImage;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Control class used to handle the Blur algorithm.
+ * 
+ * @author Sean Maloney
+ */
+public class Blur extends ImageAlgorithm{
+	/**
+	 * Performs a Guassian Blur on the given image.
+	 * @param original The original image.
+	 * @return The newly blurred image.
+	 */
+	public static BufferedImage grayScale(BufferedImage original) {
+		
+		int height = original.getHeight();
+		int width = original.getWidth();
+		
+		int[] sourceData = strip(original);
+		
+		int[] resultData = new int[sourceData.length];
+		
+		
+		long startTime = System.nanoTime();
+		for(int row = 0; row < height; row++) {
+			for(int col = 0; col < width; col++) {
+				int index = row * width + col;
+				
+				
+			}
+		}
+
+		long endTime = System.nanoTime();
+		
+		long timeTaken = endTime - startTime;
+		
+		double miliSeconds = timeTaken / 1000000.0;
+		JOptionPane.showMessageDialog(null, "Time Taken: " + miliSeconds + " (ms)");
+		
+		BufferedImage result = wrapUp(resultData, original);
+		return result;
+	}
+}
